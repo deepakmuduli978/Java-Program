8ae94e25a25e450eaec1d72c185f52df95854b01
DEEPAK MUDULI <deepakmuduli272@gmail.com>
2026-01-20 08:49:44 +0530

    Day 1 Basic Java Program

---
+++ sumfun.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+import java.util.Scanner;
+
+public class sumfun{
+    public static void main(String[] args){
+        int n1,n2;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two number:");
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        System.out.println(n1+n2);
+    }
+}
