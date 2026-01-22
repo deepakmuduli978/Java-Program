9ff23b02003e83c606f1f8e92105cd181c06dafc
DEEPAK MUDULI <deepakmuduli272@gmail.com>
2026-01-22 22:05:17 +0530

    Day 2 Pinting values;

---
+++ sumnum.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+import java.util.Scanner;
+
+public class sumnum{
+    public static void main(String[] args){
+        int n1,n2;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two number:");
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+      System.out.println("Sum of two numbers: " + (n1 + n2));
+
+    }
+}
